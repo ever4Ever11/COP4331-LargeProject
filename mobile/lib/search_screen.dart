8a39bc3d40b169ef5164732e1e3ac576dea1fe2c
Aidan Isner <ai493056@ucf.edu>
2025-03-31 00:57:06 -0400

    Added functional logout button

--- mobile/lib/search_screen.dart
+++ mobile/lib/search_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:wayfinder/login_screen.dart';
 
 class SearchScreen extends StatefulWidget {
   final Map<String, dynamic> user;
@@ -18,7 +19,23 @@ class _SearchScreenState extends State<SearchScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
-        child: Text('User ${user['userId']}: ${user['firstName']} ${user['lastName']}'),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text('User ${user['userId']}: ${user['firstName']} ${user['lastName']}'),
+            TextButton(
+              onPressed: () {
+                Navigator.of(context).pushAndRemoveUntil(
+                  MaterialPageRoute(
+                    builder: (BuildContext context) => LoginScreen(),
+                  ),
+                  (Route route) => false,
+                );
+              },
+              child: Text('Logout'),
+            )
+          ],
+        ),
       )
     );
   }
